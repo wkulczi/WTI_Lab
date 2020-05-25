1abdba09779c2cb532ba02f1d5e1dfcc1dcd4486
wkulczi <wojciechkulczak@gmail.com>
2020-05-25 20:41:42 +0200

    documented RedisQueue class removed old venv, not working

--- Lab1/wtiproj01_client.py
+++ Lab1/wtiproj01_client.py
@@ -1,21 +1,51 @@
 import redis
 
+# cnxn = RedisQueue('test', 'localhost', 6381, 0)
+
 
 class RedisQueue:
-    def __init__(self, _name, _host, _port, _db):
+    def __init__(self, _name='default', _host='localhost', _port=6381, _db=0):
+        """
+        Initialize redis queue class
+        Args:
+            _name: Name of queue in redis, default value: 'default'
+            _host: Host adress, default value: 'localhost'
+            _port: Connection port, default value: 6381
+            _db: i don't remember, sorry
+        """
         self.cnxn = redis.Redis(host=_host, port=_port, db=_db)
         self.key = '%s:%s' % ("queue", _name)
 
     def qsize(self):
+        """
+        Returns: size of list
+        """
         return self.cnxn.llen(self.key)
 
-    def empty(self):
+    def empty(self)->bool:
+        """
+        Returns: if redis queue is empty
+        """
         return self.qsize() == 0
 
     def put(self, item):
+        """
+        Pushes using redis rpush
+        Args:
+            item: Item to push
+        """
         self.cnxn.rpush(self.key, item)
 
     def get(self, block=True, timeout=None):
+        """
+        Pops using redis lpop
+        Args:
+            block: If block connection, default: True
+            timeout: Sets timeout value, default: None
+
+        Returns: Item at the leftmost.
+
+        """
         if block:
             item = self.cnxn.blpop(self.key, timeout=timeout)
         else:
@@ -27,20 +57,15 @@ class RedisQueue:
             return '(nil)'
 
     def clear(self):
+        """
+        Clears redis queue.
+        """
         self.cnxn.delete(self.key)
 
     def get_nowait(self):
+        """
+
+        Returns: ¯\_(ツ)_/¯
+
+        """
         return self.get(False)
-
-
-q = RedisQueue('test', 'localhost', 6381, 0)
-q.put('hello')
-q.put('world')
-x = q.get(block=False)
-print(x)
-x = q.get(block=False)
-print(x)
-q.put('hello')
-q.put('world')
-q.clear()
-print(q.empty())
