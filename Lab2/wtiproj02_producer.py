f515d10a98a484a6de61c02d1ae68d7f7990eb21
wkulczi <wojciechkulczak@gmail.com>
2020-05-30 19:30:44 +0200

    lab2 zad 4

--- Lab2/wtiproj02_producer.py
+++ Lab2/wtiproj02_producer.py
@@ -1,17 +1,20 @@
 import itertools
 import json
 import time
-
+import pandas as pd
 from Lab1.wtiproj01_client import RedisQueue
 
 
 def startSending():
     queue = RedisQueue(name='lab2Queue')
+    df = pd.read_table("../user_ratedmovies.dat", sep="\t")
     queue.flushdb()
-    for i in itertools.count():
-        queue.put(json.dumps({"iteration number": i}))
-        time.sleep(0.01)
-
+    try:
+        for index in df.iterrows():
+            queue.put(index[1].to_json())
+            time.sleep(0.6)
+    except KeyboardInterrupt:
+        print("Sent {0} elements of user_ratedmovies.dat".format(index[0]+1))
 
 if __name__ == "__main__":
     startSending()
--- Lab2/wtoproj02_consumer.py
+++ Lab2/wtoproj02_consumer.py
@@ -4,15 +4,27 @@ import time
 from Lab1.wtiproj01_client import RedisQueue
 
 
+def list_summary(datalist):
+    return "\nEmpty elements fetched: {0}\nNon empty elements in list: {1}\n".format(datalist.count([]),
+                                                                                     (len(datalist) - datalist.count(
+                                                                                         [])))
+
+
 def receive():
     queue = RedisQueue(name='lab2Queue')
     queue.ltrim(0, 50)
-    while True:
-        data = queue.lrange(0, 0)
-        if len(data) > 0:
-            print(json.loads(data[0]))
-            queue.ltrim(1,-1)
-        time.sleep(0.01)
+    datalist = []
+    try:
+        while True:
+            data = queue.lrange(0, 0)
+            datalist.append(data)
+            if len(data) > 0:
+                print(json.loads(data[0]))
+                queue.ltrim(1, -1)
+            time.sleep(0.25)
+
+    except KeyboardInterrupt:
+        print(list_summary(datalist))
 
 
 if __name__ == "__main__":
